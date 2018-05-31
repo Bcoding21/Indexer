cc996ff9d71628e4c9a4c08c4705b268a9187e59
Bcoding21 <brandoncole673@gmail.com>
2018-05-31 00:09:50 -0400

    Completed add function for Index class

--- Index.cpp
+++ Index.cpp
@@ -2,7 +2,14 @@
 
 void Index::add(unsigned long termId, unsigned long docId) {
 
-	// TODO
+	auto it = data.find(termId);
+	if (it == data.end()) {
+		auto docList = { docId };
+		data.emplace(termId, docList);
+	}
+	else {
+		it->second.emplace(docId);
+	}
 }
 
 void Index::write(const std::string& path) {
